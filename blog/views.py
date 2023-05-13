45fa7124873ac56e0d6fa78ffbb2ce03e7705a20
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-13 15:37:58 +0500

    init

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-# Create your views here.
+class UserView(APIView):
+    def get(self,request,id:int):
+        try:
+            user=User.objects.get(id=id)
+            return Response({ "username": user.username, "first_name": user.first_name, "last_name": user.first_name})
+        except:
+            return Response({'result':'User not found'})
