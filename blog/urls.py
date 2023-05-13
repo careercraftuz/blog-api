31ad41531f0646531107cbad16c8dff6dc07e0e7
azimabdurasulov <azimjon20050604@gmail.com>
2023-05-13 16:30:33 +0500

    add url api/posts/ to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import UserView,Users
+from .views import UserView, Users, PostsView
 
 urlpatterns = [
     path('users/<int:id>', UserView.as_view()),
     path('users/',Users.as_view()),
-    
+    path('api/posts/', PostsView.as_view())
 ]
