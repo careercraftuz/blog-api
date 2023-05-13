f1e8c775b7106bd9f2abdd077f6c4872da5d36ad
Doniyorbek <programmmerqety1@gmail.com>
2023-05-13 15:08:10 +0500

    create post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=75)
+    content = models.TextField()
+    publication_date = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, related_name=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
