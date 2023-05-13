2896230c57103ae79e31c55addbfd6c782d94f78
azimabdurasulov <azimjon20050604@gmail.com>
2023-05-13 15:13:56 +0500

    Post model created

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title   = models.CharField(max_length=255)
-    context = models.TextField(max_length=50)
+    context = models.TextField()
     publication_data = models.DateTimeField(auto_created=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
