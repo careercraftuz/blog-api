981e96919c9f398b044d7f5d42032ab7c4f9513d
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-13 15:59:30 +0500

    askdmklasldk

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.models import User
 
 # Create your views here.
+class UserView(APIView):
+    def get(self, request: Request, id: int) -> Response:
+        try:
+            user = User.objects.get(id=id)
+            data = {
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except:
+            Response({"Error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
