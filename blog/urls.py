9dfec04442e40167ab0f606c6e94b2bc595c6b00
Doniyorbek <programmmerqety1@gmail.com>
2023-05-13 16:11:00 +0500

    create user

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import UserView
 
 urlpatterns = [
-    path('users/<int:id>', UserView.as_view())
+    path('users/<int:id>', UserView.as_view()),
+    path('create/', UserView.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 class UserView(APIView):
-    def get(self,request,id:int):
+    def get(self,request:Request,id:int):
         try:
             user=User.objects.get(id=id)
             return Response({ "username": user.username, "first_name": user.first_name, "last_name": user.first_name})
         except:
             return Response({'result':'User not found'})
+    # create user
+    def post(self,request:Request):
+        data=request.data
+        user=User.objects.create_user(username=data['username'],password=data['password'])
+        user.save()
+        
+        return Response({'result':user.username})
