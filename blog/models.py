a468733823029dd7578127e952a9cf7dd8cc95f7
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2023-05-13 15:11:44 +0500

    model

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.CharField(max_length=5000)
     publication_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
