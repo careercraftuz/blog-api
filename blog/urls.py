8a0560d9c45505745073d3255193406020e5dc96
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-13 16:30:04 +0500

    added create-user/ endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserView,Users
+from .views import UserView, Users, CreateUser
 
 urlpatterns = [
     path('users/<int:id>', UserView.as_view()),
-    path('users/',Users.as_view()),
+    path('users/', Users.as_view()),
+    path('create-user/', CreateUser.as_view()),
     
 ]
