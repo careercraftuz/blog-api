dc026a55acd199ad31cc604f51aead6308855d7c
azimabdurasulov <azimjon20050604@gmail.com>
2023-05-13 16:10:49 +0500

    I changed the design

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,11 @@ class UserView(APIView):
     def get(self,request,id:int):
         try:
             user=User.objects.get(id=id)
-            return Response({ "username": user.username, "first_name": user.first_name, "last_name": user.first_name})
+            return Response({
+                "username": user.username,
+                "first_name": user.first_name, 
+                "last_name": user.first_name
+            })
         except:
             return Response({'result':'User not found'})
 class Users(APIView):
@@ -15,7 +19,11 @@ class Users(APIView):
             data=[]
             users=User.objects.all()
             for user in users:
-                data.append({ "username": user.username, "first_name": user.first_name, "last_name": user.first_name})
+                data.append({
+                    "username": user.username,
+                    "first_name": user.first_name,
+                    "last_name": user.first_name
+                })
             return Response(data)
         except:
             return Response({'result':'Users not found'})
