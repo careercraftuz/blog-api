86f8d79c0bd06154307dcd09a141fcddadd722f1
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-14 14:00:23 +0500

    added endpoint for get all posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import UserView, Users, CreateUser, PostsView
 urlpatterns = [
     path('users/<int:id>', UserView.as_view()),
     path('users/', Users.as_view()),
-    path('create-user/', CreateUser.as_view()),
+    path('users/', CreateUser.as_view()),
     path('posts/', PostsView.as_view()),
 ]
