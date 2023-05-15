cb876634e2d3c1040c1f88221c7b53e8dfbb6837
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-15 15:35:15 +0500

    for UpdatePost added url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserView, Users, CreateUser, PostsView
+from .views import UserView, Users, CreateUser, PostsView,UpdatePost
 
 urlpatterns = [
     path('users/<int:id>', UserView.as_view()),
     path('users/', Users.as_view()),
     path('users/', CreateUser.as_view()),
     path('posts/', PostsView.as_view()),
+    path('update-post/<int:id>',UpdatePost.as_view()),
 ]
