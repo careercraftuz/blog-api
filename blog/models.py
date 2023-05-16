e1e48a5040f587c384b7cd84148a1e307a70eee5
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2023-05-16 10:12:17 +0500

    def str

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,13 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 class Reaction(models.Model):
     like = models.BooleanField(default=False)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.last_name
