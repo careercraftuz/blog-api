dd932919fbcea0b0372bde4aa2691446020fade6
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-18 10:35:15 +0500

    changed create user endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path('users/<int:id>', UserView.as_view()),
     path('users/', Users.as_view()),
-    path('users/', CreateUser.as_view()),
+    path('create-user/', CreateUser.as_view()),
     path('posts/', PostsView.as_view()),
     path('create-post/',CreatePostView.as_view()),
     path('update/<int:id>',UpdatePost.as_view()),
