b740911a5b941544aaa34d79a23b7a655f036b76
Abhay Kumar Gupta <abhayetwup123@gmail.com>
2023-05-23 16:22:06 +0530

    Update urls.py
    #83

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .CreateReaction import CreateReaction
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
+    path('api/create-reaction', CreateReaction.as_view(), name='create-reaction'),
 ]
