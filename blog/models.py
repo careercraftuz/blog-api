9b2260d59b4c2b334f48f620c00ad993fe303bab
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-23 08:13:04 +0500

    str

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,17 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.title[:20]}... by {self.author.username} at {self.created_at.day}/{self.created_at.month}/{self.created_at.year}'
+    
+
 
 class Reaction(models.Model):
     like = models.BooleanField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.like:
+            return f'{self.user.username} liked {self.post.title[:20]}... at {self.post.created_at.day}/{self.post.created_at.month}/{self.post.created_at.year}'
+        return f'{self.user.username} disliked {self.post.title[:20]}... at {self.post.created_at.day}/{self.post.created_at.month}/{self.post.created_at.year}'
